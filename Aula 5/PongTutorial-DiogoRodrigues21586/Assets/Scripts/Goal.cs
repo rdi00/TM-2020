f5ea7b68c79c7a43c24cbfe13e559aa4b4ed0800
rdi00 <61155306+rdi00@users.noreply.github.com>
2020-03-25 18:17:30 +0000

    DiogoRodrigues21586
    aula5

---
+++ Aula 5/PongTutorial-DiogoRodrigues21586/Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Goal : MonoBehaviour
+{
+ private void OntriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ballTag"))
+        {
+            GameObject.Find("gameManagerObj").GetComponent<manager>().Player1Scored();
+        }
+    }
+}
---
+++ Aula 5/PongTutorial-DiogoRodrigues21586/Assets/Scripts/manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Score UI")]
+    public GameObject playertext;
+
+    private int playerScore;
+
+    public void Player1Scored()
+    {
+        playerScore++;
+        playertext.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        ball.GetComponent<Ball>().increaseSpeed();
+    }
+
+    public void Reset()
+    {
+        playerScore = 0;
+        playertext.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+    }
+
+}
